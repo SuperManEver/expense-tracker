bef8331fd37b48e71d12ae800283dfab23212b06
Nik Luparev <nikita.luparev@gmail.com>
2023-11-20 23:46:53 +0500

    stubs for pages

--- app/components/navbar/main-navbar/index.tsx
+++ app/components/navbar/main-navbar/index.tsx
@@ -1,22 +1,22 @@
 // ui
-import Link from '@/app/components/navbar/link';
-import Button from '@/app/components/ui/button';
+import Link from '@/app/components/navbar/link'
+import Button from '@/app/components/ui/button'
 
 // styles
-import css from './styles.module.css';
+import css from './styles.module.css'
 
 function MainNavbar() {
   return (
     <ul className={css.root}>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
-      <Link href="/login">Login</Link>
+      <Link href="/sign-in">Login</Link>
 
       <Link href="/login">
         <Button variant="bordered">Get Started</Button>
       </Link>
     </ul>
-  );
+  )
 }
 
-export default MainNavbar;
+export default MainNavbar
