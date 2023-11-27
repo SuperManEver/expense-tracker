732390ce44ce8f167c64657b580a2eaf11b30889
Nik Luparev <nikita.luparev@gmail.com>
2023-11-27 12:31:42 +0500

    dashboard basic layout

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,5 +1,12 @@
+import css from './styles.module.scss'
+
 function OverviewPage() {
-  return <div>Dashboard Main Page</div>
+  return (
+    <div className={css.root}>
+      <aside className={css.sidebar}>Sidebar</aside>
+      <section className={css.contentArea}>Dashboard Main Page</section>
+    </div>
+  )
 }
 
 export default OverviewPage
