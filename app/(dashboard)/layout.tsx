ce9d643de77d0497b68236c98db8dbefad223aec
Nik Luparev <nikita.luparev@gmail.com>
2023-11-27 13:30:56 +0500

    basic styling for navigation

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,13 @@
+// vendor
+import cn from 'clsx'
+
 // ui
 import Link from 'next/link'
+import {
+  BanknotesIcon,
+  ChartBarIcon,
+  FolderOpenIcon,
+} from '@heroicons/react/24/outline'
 
 // styles
 import css from './styles.module.scss'
@@ -12,10 +20,25 @@ function DashboardLayout({ children }: IProps) {
   return (
     <div className={css.root}>
       <aside className={css.sidebar}>
-        <ul>
-          <Link href="/overview">Home</Link>
-          <Link href="/expenses">Expenses</Link>
-          <Link href="/categories">Categories</Link>
+        <ul className={css.navigation}>
+          <Link href="/overview">
+            <div className={cn(css.navItem, css.active)}>
+              <ChartBarIcon className={css.icon} />
+              <p className={css.title}>Home</p>
+            </div>
+          </Link>
+          <Link href="/expenses">
+            <div className={css.navItem}>
+              <BanknotesIcon className={css.icon} />
+              <p className={css.title}>Expenses</p>
+            </div>
+          </Link>
+          <Link href="/categories">
+            <div className={css.navItem}>
+              <FolderOpenIcon className={css.icon} />
+              <p className={css.title}>Categories</p>
+            </div>
+          </Link>
         </ul>
       </aside>
       <section className={css.contentArea}>{children}</section>
