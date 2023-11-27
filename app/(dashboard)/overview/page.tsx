32ef856314fe265b9d120fbe245999c77e1dde81
Nik Luparev <nikita.luparev@gmail.com>
2023-11-27 12:36:17 +0500

    stub page for cateogies

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,9 +1,19 @@
+// ui
+import Link from 'next/link'
+
+// styles
 import css from './styles.module.scss'
 
 function OverviewPage() {
   return (
     <div className={css.root}>
-      <aside className={css.sidebar}>Sidebar</aside>
+      <aside className={css.sidebar}>
+        <ul>
+          <Link href="/overview">Home</Link>
+          <Link href="/expenses">Expenses</Link>
+          <Link href="/categories">Categories</Link>
+        </ul>
+      </aside>
       <section className={css.contentArea}>Dashboard Main Page</section>
     </div>
   )
